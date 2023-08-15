86c7a999e6356a58e43d03b672560491b6085137
Ric <ricomainah@gmail.com>
2023-08-15 14:17:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints "_putchar" followed by a new line using _putchar.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* using _putchar prototype */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}

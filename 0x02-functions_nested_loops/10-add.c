12277df7e27a575c5c9416d4bee4c14e52738505
Ric <ricomainah@gmail.com>
2023-08-17 09:51:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Add to numbers
+ *@a: first number
+ *
+ * @b:  second number
+ *
+ * Return: Always sum
+ */
+int add(int a, int b)
+{
+	/* decalring and innitializing sum */
+	int sum = a + b;
+
+	/*printing sum */
+
+	return (sum);
+}

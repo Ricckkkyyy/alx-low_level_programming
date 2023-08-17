aba8145d5e9331ef731ef5f7d649ec9c3d1555d5
Ric <ricomainah@gmail.com>
2023-08-17 10:20:29 -0700

    poditivr_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Tests the positive_or_negative function with a case of 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}

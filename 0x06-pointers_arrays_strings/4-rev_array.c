6170108c657f3eb35b896655c862b29c6f24f633
Ric <ricomainah@gmail.com>
2023-08-24 10:07:33 -0700

    corrrection 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * reverse_array - reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
  */
-void print_rev(char *s)
+void reverse_array(int *a, int n)
 {
-	int len = 0, i;
+	int i, j, tmp;
 
-	while (s[i++])
-		len++;
+	j = n - 1;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+	}
 }
